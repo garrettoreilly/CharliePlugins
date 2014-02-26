e970dec4f0be42a1c7a8bcc16efb52588d94498e
Thomas Wojnar <wojnarthomas@gmail.com>
2014-02-25 22:43:33 -0500

    Second test. Fix to issues involving jar files.

--- test/charlie/bs/section1/Test01_12_06.java
+++ test/charlie/bs/section1/Test01_12_06.java
@@ -6,16 +6,24 @@
 
 package charlie.bs.section1;
 
+import charlie.advisor.Advisor;
+import charlie.card.Card;
+import static charlie.card.Card.Suit.*;
+import charlie.card.Hand;
+import charlie.card.Hid;
+import static charlie.dealer.Seat.*;
+import charlie.util.Play;
+import static charlie.util.Play.*;
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
- * @author Thomas Wojnar
+ * @author Thomas Wojnar, Garrett Oreilly
  */
 public class Test01_12_06 {
     
@@ -43,4 +51,21 @@ public class Test01_12_06 {
     //
     // @Test
     // public void hello() {}
+    @Test
+    public void test01()
+    {
+        Hid hid = new Hid(YOU, 0.0, 0.0);
+        Card card1 = new Card(11, HEARTS);
+        Card card2 = new Card(8, DIAMONDS);
+        Card card3 = new Card(7, DIAMONDS);
+        Card upCard = new Card(5, SPADES);
+        Hand userHand = new Hand(hid);
+        userHand.hit(card1);
+        userHand.hit(card2);
+        userHand.hit(card3);
+        Advisor advisor = new Advisor();
+        Play result = advisor.advise(userHand, upCard);
+        Play expected = STAY;
+        assertEquals(result, expected);
+    }
 }
