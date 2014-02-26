979ce0ba00794a03256c18ffd28cc395a53d6e7c
Thomas Wojnar <wojnarthomas@gmail.com>
2014-02-25 21:58:59 -0500

    Revert "Fixes to Advisor/Basic Strategy, Beginning of Testing."
    This reverts commit c8bc5fe8d002a687c025640586a828d354f6a874.

--- src/charlie/advisor/BasicStrategy.java
+++ src/charlie/advisor/BasicStrategy.java
@@ -17,19 +17,19 @@ import static charlie.util.Play.*;
  * @author Thomas Wojnar, Garrett Oreilly
  */
 public class BasicStrategy {
-    public static Play getPlay(Hand inHand, Card inCard)
+    public Play getPlay(Hand inHand, Card inCard)
     {   
         int[] handValues = inHand.getValues();
         int hardHandValue = handValues[Constant.HAND_LITERAL_VALUE];
         Play returnedPlay = NONE;
         
-        if (inHand.getCard(0).value() == inHand.getCard(1).value() && inHand.size() == 2) {
+        if(inHand.getCard(0).value() == inHand.getCard(1).value() && inHand.size() == 2) {
             returnedPlay = sameCard(inHand, inCard);
         }
-        else if (handValues[0] != handValues[1] && inHand.size() == 2) {
+        else if(handValues[0] != handValues[1] && inHand.size() == 2) {
             returnedPlay = aceHand(inHand, inCard);
         }
-        else if (hardHandValue >= 5 && hardHandValue <= 11) {
+        else if(hardHandValue >= 5 && hardHandValue <= 11) {
             returnedPlay = lowerHard(inHand, inCard);
         }
         else {
@@ -38,100 +38,100 @@ public class BasicStrategy {
         return returnedPlay;
     }
     
-    private static Play sameCard(Hand inHand, Card inCard)
+    public Play sameCard(Hand inHand, Card inCard)
     {
         int cardValue = inHand.getCard(0).value();
         int upCardValue = inCard.value();
-        if (upCardValue == 1) {
+        if(upCardValue == 1) {
             upCardValue = 11;
         }
         Play returnedPlay = SPLIT;
-        if (cardValue == 5 && (upCardValue >= 2 && upCardValue <= 9)) {
+        if(cardValue == 5 && (upCardValue >= 2 && upCardValue <= 9)) {
             returnedPlay = DOUBLE_DOWN;
         }
-        else if ((cardValue >= 2 && cardValue <= 7) && (upCardValue >= 8 && upCardValue <= 11)) {
+        else if((cardValue >= 2 && cardValue <= 7) && (upCardValue >= 8 && upCardValue <= 11)) {
             returnedPlay = HIT;
         }
-        else if (cardValue == 4 && ((upCardValue >= 2 && upCardValue <= 4) || upCardValue == 7)) {
+        else if(cardValue == 4 && ((upCardValue >= 2 && upCardValue <= 4) || upCardValue == 7)) {
             returnedPlay = HIT;
         }
-        else if (cardValue == 6 && upCardValue == 7) {
+        else if(cardValue == 6 && upCardValue == 7) {
             returnedPlay = HIT;
         }
-        else if (cardValue == 9 && (upCardValue == 7 || upCardValue == 10 || upCardValue == 11)) {
+        else if(cardValue == 9 && (upCardValue == 7 || upCardValue == 10 || upCardValue == 11)) {
             returnedPlay = STAY;
         }
-        else if (cardValue == 10) {
+        else if(cardValue == 10) {
             returnedPlay = STAY;
         }
         return returnedPlay;
     }
     
-    private static Play aceHand(Hand inHand, Card inCard)
+    public Play aceHand(Hand inHand, Card inCard)
     {
         int cardValue1 = inHand.getCard(0).value();
         int cardValue2 = inHand.getCard(1).value();
         int cardValueToUse = 0;
-        if (cardValue1 == 1) {
+        if(cardValue1 == 1) {
             cardValueToUse = cardValue2;
         } else {
             cardValueToUse = cardValue1;
         }
         int upCardValue = inCard.value();
-        if (upCardValue == 1) {
+        if(upCardValue == 1) {
             upCardValue = 11;
         }
         Play returnedPlay = HIT;
-        if ((cardValueToUse == 2 || cardValueToUse == 3) && (upCardValue == 5 || upCardValue == 6)) {
+        if((cardValueToUse == 2 || cardValueToUse == 3) && (upCardValue == 5 || upCardValue == 6)) {
             returnedPlay = DOUBLE_DOWN;
         }
-        else if ((cardValueToUse == 4 || cardValueToUse == 5) && (upCardValue >= 4 || upCardValue <= 6)) {
+        else if((cardValueToUse == 4 || cardValueToUse == 5) && (upCardValue >= 4 || upCardValue <= 6)) {
             returnedPlay = DOUBLE_DOWN;
         }
-        else if ((cardValueToUse == 6 || cardValueToUse == 7) && (upCardValue >= 3 || upCardValue <= 6)) {
+        else if((cardValueToUse == 6 || cardValueToUse == 7) && (upCardValue >= 3 || upCardValue <= 6)) {
             returnedPlay = DOUBLE_DOWN;
         }
-        else if ((cardValueToUse == 7)  && (upCardValue == 2 || upCardValue == 7 || upCardValue == 8)) {
+        else if((cardValueToUse == 7)  && (upCardValue == 2 || upCardValue == 7 || upCardValue == 8)) {
             returnedPlay = STAY;
         }
-        else if (cardValueToUse >= 8 && cardValueToUse <= 10) {
+        else if(cardValueToUse >= 8 && cardValueToUse <= 10) {
             returnedPlay = STAY;
         }
         return returnedPlay;
     }
     
-    private static Play lowerHard(Hand inHand, Card inCard)
+    public Play lowerHard(Hand inHand, Card inCard)
     {
         int handValue = inHand.getValues()[Constant.HAND_LITERAL_VALUE];
         int upCardValue = inCard.value();
-        if (upCardValue == 1) {
+        if(upCardValue == 1) {
             upCardValue = 11;
         }
         Play returnedPlay = HIT;
-        if (handValue == 9 && (upCardValue >= 3 && upCardValue <= 6)) {
+        if(handValue == 9 && (upCardValue >= 3 && upCardValue <= 6)) {
             returnedPlay = DOUBLE_DOWN;
         }
-        else if (handValue == 10 && (upCardValue >= 2 && upCardValue <= 9)) {
+        else if(handValue == 10 && (upCardValue >= 2 && upCardValue <= 9)) {
             returnedPlay = DOUBLE_DOWN;
         }
-        else if (handValue == 11 && (upCardValue >= 2 && upCardValue <= 10)) {
+        else if(handValue == 11 && (upCardValue >= 2 && upCardValue <= 10)) {
             returnedPlay = DOUBLE_DOWN;
         }
         return returnedPlay;
     }
     
-    private static Play higherHard(Hand inHand, Card inCard)
+    public Play higherHard(Hand inHand, Card inCard)
     {
         int handValue = inHand.getValues()[Constant.HAND_LITERAL_VALUE];
         int upCardValue = inCard.value();
-        if (upCardValue == 1) {
+        if(upCardValue == 1) {
             upCardValue = 11;
         }
         Play returnedPlay = STAY;
-        if (handValue == 12 && (upCardValue == 2 || upCardValue == 3)) {
+        if(handValue == 12 && (upCardValue == 2 || upCardValue == 3)) {
             returnedPlay = HIT;
         }
-        else if ((handValue >= 12 && handValue <= 16) && (upCardValue >= 7 && upCardValue <= 11)) {
+        else if((handValue >= 12 && handValue <= 16) && (upCardValue >= 7 && upCardValue <= 11)) {
             returnedPlay = HIT;
         }
         return returnedPlay;
